e1f580b9895c9b8e53b781b86460a38ba743343a
Anton <anvlshmelev@edu.hse.ru>
2026-01-22 19:48:16 +0300

    upd: icons component

---
+++ frontend/src/app/app.icons.ts
@@ -0,0 +1,6 @@
+export const iconsConfig = {
+  assetsPath: "icons",
+  icons: [
+    "logo",
+  ] as const,
+};
---
+++ frontend/src/app/shared/components/icon/icon.ts
@@ -0,0 +1,25 @@
+import { iconsConfig, IconService } from "./icon.service";
+import { booleanAttribute, ChangeDetectionStrategy, Component, inject, input } from "@angular/core";
+import { SafeHtml } from "@angular/platform-browser";
+
+@Component({
+  selector: "app-icon",
+  imports: [],
+  template: "",
+  styleUrl: "./icon.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    "[innerHtml]": "inner",
+    "[class.filled]": "filled()",
+  },
+})
+export class Icon {
+  private readonly iconService = inject(IconService);
+
+  icon = input.required<typeof iconsConfig.icons[number]>();
+  filled = input(false, { transform: booleanAttribute });
+
+  get inner(): SafeHtml {
+    return this.iconService.get(this.icon());
+  }
+}
