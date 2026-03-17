fa4204a220d5b105346be60268e50c45047faaa9
Fedos <starfedto@gmail.com>
2026-03-17 16:24:24 +0300

    bug fix: wrong model name

--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -8,7 +8,7 @@ until ollama list > /dev/null 2>&1; do
 done
 
 echo "Warming up model..."
-ollama run gemma3n "Hello" --nowordwrap > /dev/null 2>&1
+ollama run hf.co/unsloth/gemma-3n-E4B-it-GGUF:Q4_K_XL "Hello" --nowordwrap > /dev/null 2>&1
 
 echo "Model ready!"
 wait $OLLAMA_PID
